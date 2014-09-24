50bf37f401de889eff59373d2221e47b44470a68
luchen825 <lucychen28@gmail.com>
2014-09-24 09:58:11 -0700

    Added Chemotaxis

--- build-tmp/source/Chemotaxis.java
+++ build-tmp/source/Chemotaxis.java
@@ -1,6 +1,22 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class Chemotaxis extends PApplet {
+
 Bacteria [] one;
 Food [] two; 
-void setup()   
+public void setup()   
 {   
 	size(600,600);
 	one = new Bacteria[20];
@@ -14,7 +30,7 @@ void setup()
 		two[k] = new Food((int)(Math.random()*600),(int)(Math.random()*600)); 
 	}
 }   
-void draw()   
+public void draw()   
 {   
 	background(0);
 	for(int j = 0; j < one.length; j++)
@@ -56,7 +72,7 @@ void draw()
 		}
 	}   
 }  
-void mousePressed()
+public void mousePressed()
 {
 	for(int k = 0; k < two.length; k++)
 	{
@@ -76,12 +92,12 @@ class Bacteria
 		color1 = (int)(Math.random()*127) + 70;
 		color2 = (int)(Math.random()*127) + 70;
 	} 
-	void walk()
+	public void walk()
 	{
 		newX = newX + (int)(Math.random()*5)-2;
 		newY = newY + (int)(Math.random()*5)-2;
 	}
-	void show()
+	public void show()
 	{
 		fill(0,color1,color2);
 		ellipse(newX,newY,20,20);
@@ -96,10 +112,19 @@ class Food
 		fX = a;
 		fY = b;
 	}
-	void show()
+	public void show()
 	{
 		noStroke();
 		fill(255,0,0);
 		ellipse(fX,fY,15,15);
 	}
 }  
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "Chemotaxis" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
+}
